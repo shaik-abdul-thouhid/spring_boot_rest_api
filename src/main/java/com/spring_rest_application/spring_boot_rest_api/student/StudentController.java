fd134c4635027ddd574756d5e482c8bc6e8ee148
Shaik Abdul Thouhid <shaikabdulthouhid.01@gmail.com>
2023-04-19 17:45:34 +0530

    delete and put methods implemented

--- src/main/java/com/spring_rest_application/spring_boot_rest_api/student/StudentController.java
+++ src/main/java/com/spring_rest_application/spring_boot_rest_api/student/StudentController.java
@@ -2,10 +2,14 @@ package com.spring_rest_application.spring_boot_rest_api.student;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,6 +29,20 @@ public class StudentController {
 	@PostMapping("")
 	public void registerNewStudent(@RequestBody Student student) {
 		studentService.addNewStudent(student);
+	}
+
+	@DeleteMapping(path = "{studentId}")
+	public void deleteStudent(@PathVariable("studentId") Long id) {
+		studentService.deleteStudent(id);
+	}
+
+	@PutMapping(path = "{studentId}")
+	public void updateStudent(
+		@PathVariable("studentId") Long studentId, 
+		@RequestParam(required = false) String name, 
+		@RequestParam(required = false) String email
+	) {
+		this.studentService.updateStudent(studentId, name, email);
 	}
 
 }
--- src/main/java/com/spring_rest_application/spring_boot_rest_api/student/StudentService.java
+++ src/main/java/com/spring_rest_application/spring_boot_rest_api/student/StudentService.java
@@ -1,9 +1,11 @@
 package com.spring_rest_application.spring_boot_rest_api.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
@@ -27,4 +29,34 @@ public class StudentService {
 		
 		this.studentRepository.save(student);
 	}
+
+	public void deleteStudent(Long id) {
+		boolean exists = studentRepository.existsById(id);
+
+		if (!exists) {
+			throw new IllegalStateException("Student with id " + id + " does not exits");
+		}
+		studentRepository.deleteById(id);
+	}
+
+	@Transactional
+	public void updateStudent(Long studentId, String name, String email) {
+		Student student = this.studentRepository.findById(studentId)
+							.orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " does not exists"));
+
+		if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+			student.setName(name);
+		}
+
+		if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+			Optional<Student> studentOptional = this.studentRepository.findStudentByEmail(email);
+
+			if (studentOptional.isPresent()) {
+				throw new IllegalStateException("email taken");
+			}
+
+			student.setEmail(email);
+		}
+	}
+
 }
